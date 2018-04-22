1f952bfa866f6507cc38123deeb2c01eb235ba06
Aurélia <aurelia96@free.fr>
2018-04-23 01:38:09 +0200

    Changement design et adaptation, retrait mdp

--- Neurotest/Assets/Scripts/LogIn.cs
+++ Neurotest/Assets/Scripts/LogIn.cs
@@ -8,25 +8,18 @@ using System.Text.RegularExpressions;
 public class LogIn : MonoBehaviour {
 
     public InputField username;
-    public InputField password;
-
-    private String[] lines;
-    private string decryptedPass;
 
     private string usernamestring;
-    private string passwordstring;
 
     public void LogInButton()
     {
         bool UN = false;
-        bool PW = false;
 
         if (usernamestring != "")
         {
             if (!System.IO.File.Exists(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/")) {
                 UN = true;
                 print("Le nom est " + usernamestring);
-                lines = System.IO.File.ReadAllLines(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/" + usernamestring + ".txt");
             }
             else {
                 Debug.LogWarning("Username incorrect");
@@ -36,38 +29,20 @@ public class LogIn : MonoBehaviour {
             Debug.LogWarning("Username field empty");
         }
 
-        if (passwordstring != "") {
-            if (System.IO.File.Exists(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/" + usernamestring + ".txt")) {
 
-                int i = 1;
-                foreach (char c in lines[1]) {
-                    i++;
-                    char Decrypted = (char)(c / i);
-                    decryptedPass += Decrypted.ToString();
-                }
-
-                if (passwordstring == decryptedPass) {
-                    PW = true;
-                } else {
-                    Debug.LogWarning("Password Is invalid");
-                }
-            } else {
-                Debug.LogWarning("Password Is invalid");
-            }
-
-        } else {
-            Debug.LogWarning("Password field empty");
-        }
-
-        if (UN == true && PW == true) {
+        if (UN == true) {
             print("LogIn Okay");
         }
     }
 
+   public void Keyboard()
+    {
+        TouchScreenKeyboard.Open("", TouchScreenKeyboardType.Default);
+    }
 
-        // Update is called once per frame
-        void Update () {
+   // Update is called once per frame
+    void Update () {
         usernamestring = username.text;
-        passwordstring = password.text;
+
     }
 }
--- Neurotest/Assets/Scripts/SignIn.cs
+++ Neurotest/Assets/Scripts/SignIn.cs
@@ -8,17 +8,12 @@ using System.Text.RegularExpressions;
 public class SignIn : MonoBehaviour {
 
 	public InputField username;
-	public InputField password;
 
 	private string usernamestring;
-	private string passwordstring;
-
-	private string form;
 
 
 	public void SignInButton() {
 		bool UN = false;
-		bool PW = false;
         
 		if (usernamestring != "") {
 			if (!System.IO.File.Exists(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/")) {
@@ -31,34 +26,11 @@ public class SignIn : MonoBehaviour {
 				Debug.LogWarning("Username field empty");
 		}
 
-		if (passwordstring != "") {
-			if (passwordstring.Length > 5) {
-					PW = true;
-			} else {
-					Debug.LogWarning("Password must be at least a 6 characters long");
-			}
-		} else {
-				Debug.LogWarning("Password field empty");
-		}
+	
 
-		if (UN == true && PW == true) {
-			bool Clear = true;
-			int i = 1;
-			foreach(char c in passwordstring) {
-				if (Clear) {
-                    passwordstring = "";
-					Clear = false;
-				}
-				i++;
-				char Encrypted = (char)(c * i);
-                passwordstring += Encrypted.ToString();
-			}
-			form = (usernamestring + Environment.NewLine+ passwordstring);
+		if (UN == true) {
+
             System.IO.Directory.CreateDirectory(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/");
-            System.IO.File.WriteAllText(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/"+ usernamestring + "/" + usernamestring +".txt", form);
-            //username.GetComponentsInChildren<Text>().Set("");
-            //password.GetComponentsInChildren<Text>().Set("");
-            print(usernamestring);
 
             print("Register Okay");
 		}
@@ -68,6 +40,5 @@ public class SignIn : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         usernamestring = username.text;
-        passwordstring = password.text;
 	}
 }
