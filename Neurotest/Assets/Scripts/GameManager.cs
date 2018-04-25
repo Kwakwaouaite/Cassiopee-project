b21b2e54a202341e5bd4f28e21d00f7f0eab473f
Pierrick Pamart <pierrick.pamart@telecom-sudparis.eu>
2018-04-25 18:55:31 +0200

    Added the difficulty, changing the serie displayed

--- Neurotest/Assets/Scripts/GameManager.cs
+++ Neurotest/Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Text;
 
 public class GameManager : MonoBehaviour {
 
@@ -12,10 +13,16 @@ public class GameManager : MonoBehaviour {
 
     private List<GameObject> listPoints;
     private long current;
+    private string currentPlayer;
 
     // Use this for initialization
     void Start () {
 
+        currentPlayer = PlayerPrefs.GetString("current_player");
+        if (currentPlayer == "")
+        {
+            Debug.Log("<color=red>Error: </color><b>current_player </b>not found.");
+        }
         generatePointList();
         current = 0;
 		
@@ -57,12 +64,36 @@ public class GameManager : MonoBehaviour {
 
     private void generatePointList()
     {
+        char[] alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+
         listPoints = new List<GameObject>();
+        string difficulty = PlayerPrefs.GetString("current_difficulty");
+        if (!(difficulty == "easy" || difficulty == "medium" || difficulty == "hard"))
+        {
+            difficulty = "easy";
+            Debug.Log("<color=yellow>Warning: </color>difficulty not found, set at default: " + difficulty);
+        }
         for (int i = 0; i < pointPositions.Length; i++)
         {
             GameObject newPoint = Instantiate(prefabPoint);
             newPoint.transform.SetPositionAndRotation(new Vector3(pointPositions[i].x, pointPositions[i].y, -20), Quaternion.identity);
-            newPoint.name = "Point " + i.ToString();
+            if (difficulty == "easy")
+            {
+                newPoint.name = "Point " + i.ToString();
+            } else if (difficulty == "medium")
+            {
+                newPoint.name = "Point " + alphabet[i];
+            } else
+            {
+                if (i%2 == 0)
+                {
+                    newPoint.name = "Point " + ((i/2) + 1).ToString();
+                } else
+                {
+                    newPoint.name = "Point " + alphabet[(i-1)/ 2];
+                }
+            }
+            
             newPoint.SetActive(true);
             listPoints.Add(newPoint);
         }
