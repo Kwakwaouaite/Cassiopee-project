a5508e6f9bf40c9e96991496cdf01445c43a4f6d
Aurélia <aurelia96@free.fr>
2018-04-25 18:43:08 +0200

    Ajout des PlayerPrefs et de l'option size

---
+++ Neurotest/Assets/Options.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Options : MonoBehaviour {
+
+    public void AdjustSize(float newSize)
+    {
+        PlayerPrefs.SetFloat(PlayerPrefs.GetString("current_player")+"_option_size", newSize);
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Neurotest/Assets/Scripts/SignIn.cs
+++ Neurotest/Assets/Scripts/SignIn.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 using System;
 using System.Text.RegularExpressions;
 
+
 public class SignIn : MonoBehaviour {
 
 	public InputField username;
@@ -21,8 +22,23 @@ public class SignIn : MonoBehaviour {
         
 		if (usernamestring != "") {
 			if (!System.IO.Directory.Exists(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/")) {
+                // création des dossiers
                 System.IO.Directory.CreateDirectory(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/");
+                System.IO.Directory.CreateDirectory(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/easy/");
+                System.IO.Directory.CreateDirectory(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/medium/");
+                System.IO.Directory.CreateDirectory(@"C:/Dev/Cassiopee-project/Neurotest/Assets/Users/" + usernamestring + "/hard");
+
+                // creation et mise à jour des sharedpref
+                PlayerPrefs.SetInt(usernamestring+"_easy_level", 0);
+                PlayerPrefs.SetInt(usernamestring + "_medium_level", 0);
+                PlayerPrefs.SetInt(usernamestring + "_hard_level", 0);
+
+                PlayerPrefs.SetString("current_player", usernamestring);
+                
+                // clean inputfield
                 username.text = "";
+
+                // changement de scene
                 signin.SetActive(false);
                 secondmenu.SetActive(true);
             }
